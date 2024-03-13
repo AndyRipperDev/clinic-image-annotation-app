6dc1c56e3f8e2f3e494c3bbcf4c25ff11a423645
Ondřej Filip <zoomblick@gmail.com>
2024-03-13 09:25:54 +0100

    Add support for annotations on client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
 import './index.css';
 import App from './App';
 import HomePage from './pages/Home/HomePage';
